c46600d5927471a377e49e634f415de8eb06f1d4
Andrew Shearer <andrewshearerphotography@gmail.com>
2019-08-12 18:16:26 -0700

    Added more advanced array method files

---
+++ concepts/forEach-loop-3.js
@@ -0,0 +1,24 @@
+// Complete the below questions using this array:
+const array = [
+	{ username: "john", team: "red", score: 5, items: ["ball", "book", "pen"] },
+	{ username: "becky", team: "blue", score: 10, items: ["tape", "backpack", "pen"] },
+	{ username: "susy", team: "red", score: 55, items: ["ball", "eraser", "pen"] },
+  	{ username: "tyson", team: "green", score: 1, items: ["book", "pen"] },
+];
+
+// Create an array using forEach that has all the usernames
+let nameArray = [];
+array.forEach(function(user){
+	let {username} = user;
+	nameArray.push(username);
+})
+console.log('forEach array names', nameArray);
+
+// Create an array using forEach that has all the usernames with a "!" to each of the usernames
+let nameArrayExclamation = []
+array.forEach(user => {
+	let {username} = user;
+	username = username + "!";
+	nameArrayExclamation.push(username);
+})
+console.log('forEach array names with exclamation', nameArrayExclamation);
---
+++ concepts/reduce-3.js
@@ -0,0 +1,24 @@
+// Complete the below questions using this array:
+const array = [
+	{ username: "john", team: "red", score: 5, items: ["ball", "book", "pen"] },
+	{ username: "becky", team: "blue", score: 10, items: ["tape", "backpack", "pen"] },
+	{ username: "susy", team: "red", score: 55, items: ["ball", "eraser", "pen"] },
+  	{ username: "tyson", team: "green", score: 1, items: ["book", "pen"] },
+];
+
+// Find out the total score of all users using 
+// array / score
+const totalUserScore = array.reduce(function (total, user) {
+	return total + user.score;
+}, 0);
+console.log('sum of all users scores', totalUserScore);
+
+// Arrow function of above
+const totalUserScoreArrow = array.reduce((total, user) => {
+	return total + user.score;
+}, 0);
+console.log('sum of all users scores using arrow function', totalUserScoreArrow);
+
+// Arrow function shorthand of above
+const totalUserScoreArrowShort = array.reduce((total, user) => total + user.score, 0);
+console.log('sum of all users scores using arrow function shorthand', totalUserScoreArrowShort);
